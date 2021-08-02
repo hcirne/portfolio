d3b1186e88cd3888f49cca0d46ec38605b5c70ef
Henrique Cirne <henrique@MBP-de-Henrique.home>
2021-08-02 22:48:00 +0100

    Intro section started

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,19 +1,17 @@
 export default function Navbar() {
   return (
-    <nav className="bg-light">
+    <nav className="bg-light absolute w-screen">
       <div className="max-w-6xl mx-auto px-4">
         <div className="flex justify-between">
           {/* Logo */}
-          <div className="flex items-center">
-            <a href="/">
-              <p
-                className="text-dark text-3xl sm:text-5xl font-black italic py-3 px-5
+          <a href="/">
+            <p
+              className="text-dark text-3xl sm:text-5xl font-black italic py-3 px-5
 															transition duration-300 ease-in-out transform-gpu hover:scale-105"
-              >
-                hc
-              </p>
-            </a>
-          </div>
+            >
+              hc
+            </p>
+          </a>
           <div className="flex items-center space-x-5 md:space-x-10">
             {/* Work Link */}
             <div>
--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Intro from "./Components/Intro";
 import Navbar from "./Components/Navbar";
 import "./index.css";
 
 ReactDOM.render(
   <React.StrictMode>
     <Navbar />
+    <Intro />
   </React.StrictMode>,
   document.getElementById("root")
 );
