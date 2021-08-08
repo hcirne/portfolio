fe18caf3a6c95cf16b0ca66ca6dd4a9beab69328
Henrique Cirne <henrique@MBP-de-Henrique.home>
2021-08-08 20:37:27 +0100

    intro fixed, projects section started

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Intro from "./Components/Intro";
 import Navbar from "./Components/Navbar";
+import Projects from "./Components/Projects";
 import "./index.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Navbar />
+		<Navbar />
     <Intro />
+    <Projects />
   </React.StrictMode>,
   document.getElementById("root")
 );
